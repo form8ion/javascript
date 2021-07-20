39c529a87340811fd7064df0e9cc7a23ecaf7342
Matt Travi <programmer@travi.org>
2021-07-19 23:19:58 -0500

    refactor(coverage): grouped coverage related details a bit better

---
+++ src/coverage/nyc.js
@@ -0,0 +1,29 @@
+import {promises} from 'fs';
+
+export default async function ({projectRoot, vcs, visibility}) {
+  await promises.writeFile(
+    `${projectRoot}/.nycrc`,
+    JSON.stringify({
+      extends: '@istanbuljs/nyc-config-babel',
+      reporter: ['lcov', 'text-summary', 'html'],
+      exclude: ['src/**/*-test.js', 'test/', 'thirdparty-wrappers/', 'vendor/']
+    })
+  );
+
+  return {
+    devDependencies: ['cross-env', 'nyc', '@istanbuljs/nyc-config-babel'],
+    vcsIgnore: {files: [], directories: ['/coverage/', '/.nyc_output/']},
+    eslint: {ignore: {directories: ['/coverage/']}},
+    badges: {
+      status: {
+        ...vcs && 'github' === vcs.host && 'Public' === visibility && {
+          coverage: {
+            img: `https://img.shields.io/codecov/c/github/${vcs.owner}/${vcs.name}.svg`,
+            link: `https://codecov.io/github/${vcs.owner}/${vcs.name}`,
+            text: 'Codecov'
+          }
+        }
+      }
+    }
+  };
+}
