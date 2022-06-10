430f46021929385596dfef5584f05d59c4312736
Matt Travi <programmer@travi.org>
2022-06-10 12:14:16 -0500

    fix(deprecation): migrated from the deprecated `fs.rmdir` to `fs.rm`

--- src/coverage/nyc/remover-test.js
+++ src/coverage/nyc/remover-test.js
@@ -14,7 +14,7 @@ suite('nyc remover', () => {
     sandbox = sinon.createSandbox();
 
     sandbox.stub(fs, 'unlink');
-    sandbox.stub(fs, 'rmdir');
+    sandbox.stub(fs, 'rm');
     sandbox.stub(dependencyRemover, 'default');
   });
 
@@ -27,7 +27,7 @@ suite('nyc remover', () => {
     await removeNyc({projectRoot, packageManager});
 
     assert.calledWith(fs.unlink, `${projectRoot}/.nycrc`);
-    assert.calledWith(fs.rmdir, `${projectRoot}/.nyc_output`, {recursive: true, force: true});
+    assert.calledWith(fs.rm, `${projectRoot}/.nyc_output`, {recursive: true, force: true});
     assert.calledWith(
       dependencyRemover.default,
       {packageManager, dependencies: ['nyc', '@istanbuljs/nyc-config-babel', 'babel-plugin-istanbul']}
--- src/coverage/nyc/remover.js
+++ src/coverage/nyc/remover.js
@@ -4,7 +4,7 @@ import removeDependencies from '../../dependencies/remover';
 export default async function ({projectRoot, packageManager}) {
   await Promise.all([
     fs.unlink(`${projectRoot}/.nycrc`),
-    fs.rmdir(`${projectRoot}/.nyc_output`, {recursive: true, force: true}),
+    fs.rm(`${projectRoot}/.nyc_output`, {recursive: true, force: true}),
     removeDependencies({packageManager, dependencies: ['nyc', '@istanbuljs/nyc-config-babel', 'babel-plugin-istanbul']})
   ]);
 }
