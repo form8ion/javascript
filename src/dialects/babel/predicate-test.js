ecc8ca1e97ace67991d53ab54b6a1bb78f99a768
Matt Travi <programmer@travi.org>
2022-07-21 17:04:12 -0500

    wip(dialect): added a predicate to determine if project is liftable for babel

---
+++ src/dialects/babel/predicate-test.js
@@ -0,0 +1,32 @@
+import * as core from '@form8ion/core';
+
+import {assert} from 'chai';
+import sinon from 'sinon';
+import any from '@travi/any';
+
+import babelExists from './predicate';
+
+suite('babel predicate', () => {
+  let sandbox;
+  const projectRoot = any.string();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(core, 'fileExists');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that `true` is returned if a babel config file exists', async () => {
+    core.fileExists.withArgs(`${projectRoot}/.babelrc.json`).resolves(true);
+
+    assert.isTrue(await babelExists({projectRoot}));
+  });
+
+  test('that `false` is returned if a babel config file does not exist', async () => {
+    core.fileExists.resolves(false);
+
+    assert.isFalse(await babelExists({projectRoot}));
+  });
+});
