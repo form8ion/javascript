4148ad4c6a473d5bc46bd4527a0b9e416a9adfed
Matt Travi <programmer@travi.org>
2023-09-02 23:09:48 -0500

    feat(publishable): enabled the lifter to inject the npm badge

--- src/project-type/publishable/lifter.js
+++ src/project-type/publishable/lifter.js
@@ -1,10 +1,17 @@
 import deepmerge from 'deepmerge';
 
+import defineBadges from './badges';
 import {lift as liftProvenance} from './provenance';
 
 export default async function ({projectRoot, packageDetails}) {
+  const {name: packageName, publishConfig: {access: pacakgeAccessLevel}} = packageDetails;
+
   return deepmerge(
     await liftProvenance({packageDetails, projectRoot}),
-    {devDependencies: ['publint'], scripts: {'lint:publish': 'publint --strict'}}
+    {
+      devDependencies: ['publint'],
+      scripts: {'lint:publish': 'publint --strict'},
+      badges: defineBadges(packageName, pacakgeAccessLevel)
+    }
   );
 }
--- src/project-type/publishable/lifter.test.js
+++ src/project-type/publishable/lifter.test.js
@@ -4,11 +4,13 @@ import {afterEach, describe, expect, it, vi} from 'vitest';
 import any from '@travi/any';
 import {when} from 'jest-when';
 
+import defineBadges from './badges';
 import {lift as liftProvenance} from './provenance';
 import lift from './lifter';
 
 vi.mock('deepmerge');
 vi.mock('./provenance');
+vi.mock('./badges');
 
 describe('publishable project-type lifter', () => {
   afterEach(() => {
@@ -17,13 +19,17 @@ describe('publishable project-type lifter', () => {
 
   it('should lift the details of the package project', async () => {
     const projectRoot = any.string();
-    const packageDetails = any.simpleObject();
+    const packageName = any.word();
+    const packageAccessLevel = any.word();
+    const packageDetails = {...any.simpleObject(), name: packageName, publishConfig: {access: packageAccessLevel}};
     const provenanceResults = any.simpleObject();
     const mergedResults = any.simpleObject();
+    const badgesResults = any.simpleObject();
     when(liftProvenance).calledWith({packageDetails, projectRoot}).mockResolvedValue(provenanceResults);
+    when(defineBadges).calledWith(packageName, packageAccessLevel).mockReturnValue(badgesResults);
     when(deepmerge).calledWith(
       provenanceResults,
-      {scripts: {'lint:publish': 'publint --strict'}, devDependencies: ['publint']}
+      {scripts: {'lint:publish': 'publint --strict'}, devDependencies: ['publint'], badges: badgesResults}
     ).mockReturnValue(mergedResults);
 
     expect(await lift({projectRoot, packageDetails})).toEqual(mergedResults);
