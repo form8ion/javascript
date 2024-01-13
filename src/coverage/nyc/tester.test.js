41628949f76fa595228a60441e61352537d9aa37
Matt Travi <programmer@travi.org>
2024-01-13 00:36:58 -0600

    test(nyc): converted the predicate tests to vitest
    for #452

---
+++ src/coverage/nyc/tester.test.js
@@ -0,0 +1,29 @@
+import {fileExists} from '@form8ion/core';
+
+import {describe, it, expect, vi, afterEach} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import nycIsConfigured from './tester.js';
+
+vi.mock('@form8ion/core');
+
+describe('nyc predicate', () => {
+  const projectRoot = any.string();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return `true` if the config file exists', async () => {
+    when(fileExists).calledWith(`${projectRoot}/.nycrc`).mockResolvedValue(true);
+
+    expect(await nycIsConfigured({projectRoot})).toBe(true);
+  });
+
+  it('should return `false` if the config file does not exist', async () => {
+    when(fileExists).calledWith(`${projectRoot}/.nycrc`).mockResolvedValue(false);
+
+    expect(await nycIsConfigured({projectRoot})).toBe(false);
+  });
+});
