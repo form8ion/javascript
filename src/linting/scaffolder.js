7b864b06e0bee51c6baff62268c1b465f96e9291
Matt Travi <programmer@travi.org>
2024-01-15 22:34:53 -0600

    feat(ban-sensitive-files): stopped scaffolding due to unaddressed vulnerabilities
    BREAKING CHANGE: ban-sensitive-files is no longer scaffolded to avoid unaddressed vulnerabilities

--- src/linting/scaffolder.js
+++ src/linting/scaffolder.js
@@ -1,11 +1,5 @@
-import deepmerge from 'deepmerge';
-
-import scaffoldBanSensitiveFiles from './ban-sensitive-files.js';
 import {scaffold as scaffoldLockfileLint} from '../lockfile-lint/index.js';
 
-export default async function ({projectRoot, packageManager, registries, vcs, pathWithinParent}) {
-  return deepmerge.all(await Promise.all([
-    scaffoldLockfileLint({projectRoot, packageManager, registries}),
-    vcs ? scaffoldBanSensitiveFiles({pathWithinParent}) : {}
-  ]));
+export default async function ({projectRoot, packageManager, registries}) {
+  return scaffoldLockfileLint({projectRoot, packageManager, registries});
 }
