76e95ea681617ecb5a2a3239e540a2742f1bb752
Matt Travi <programmer@travi.org>
2024-03-16 14:37:08 -0500

    wip(yarn): defined a predicate to detect use of yarn

---
+++ src/package-managers/yarn/tester.test.js
@@ -0,0 +1,29 @@
+import {fileExists} from '@form8ion/core';
+
+import {expect, describe, it, vi, afterEach} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import npmIsUsed from './tester.js';
+
+vi.mock('@form8ion/core');
+
+describe('yarn predicate', () => {
+  const projectRoot = any.string();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return `true` is a `yarn.lock` exists', async () => {
+    when(fileExists).calledWith(`${projectRoot}/yarn.lock`).mockResolvedValue(true);
+
+    expect(await npmIsUsed({projectRoot})).toBe(true);
+  });
+
+  it('should return `false` is a `yarn.lock` does not exist', async () => {
+    when(fileExists).calledWith(`${projectRoot}/yarn.lock`).mockResolvedValue(false);
+
+    expect(await npmIsUsed({projectRoot})).toBe(false);
+  });
+});
