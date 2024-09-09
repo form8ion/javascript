ceac159c6e510c14166b4bf0d67b9cf02bae5886
Matt Travi <programmer@travi.org>
2024-09-08 20:49:41 -0400

    refactor(babel-config): leveraged the config loader

--- src/dialects/babel/config/ignore-adder.test.js
+++ src/dialects/babel/config/ignore-adder.test.js
@@ -1,28 +1,21 @@
-import {promises as fs} from 'node:fs';
-
 import any from '@travi/any';
-import {when} from 'jest-when';
-import {afterEach, describe, expect, it, vi} from 'vitest';
+import {describe, expect, it, vi} from 'vitest';
 
 import writeConfig from './write.js';
+import loadConfig from './loader.js';
 import addIgnore from './ignore-adder.js';
 
 vi.mock('node:fs');
 vi.mock('./write.js');
+vi.mock('./loader.js');
 
 describe('babel ignore adder', () => {
   const projectRoot = any.string();
 
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
-
   it('should add the provided ignore to the existing config', async () => {
     const pathToIgnore = any.string();
     const existingConfig = any.simpleObject();
-    when(fs.readFile)
-      .calledWith(`${projectRoot}/.babelrc.json`, 'utf-8')
-      .mockResolvedValue(JSON.stringify(existingConfig));
+    loadConfig.mockResolvedValue(existingConfig);
 
     await addIgnore({projectRoot, ignore: pathToIgnore});
 
@@ -33,7 +26,7 @@ describe('babel ignore adder', () => {
   it('should not update the config if no `buildDirectory` is provided', async () => {
     await addIgnore({projectRoot});
 
-    expect(fs.readFile).not.toHaveBeenCalled();
+    expect(loadConfig).not.toHaveBeenCalled();
     expect(writeConfig).not.toHaveBeenCalled();
   });
 });
