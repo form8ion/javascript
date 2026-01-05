401e52764af198b2145fc1ec330508c65fcfd91e
Matt Travi <programmer@travi.org>
2026-01-04 22:15:45 -0600

    feat(script-sorting): handle post-script ordering

--- src/package/scripts/script-comparator.js
+++ src/package/scripts/script-comparator.js
@@ -2,18 +2,23 @@ function sortTestScript() {
   return -1;
 }
 
+function isRelatedScript(a, b, prefix) {
+  return a.startsWith(prefix) && a.slice(prefix.length) === b;
+}
+
 function isPreScriptFor(a, b) {
-  return a.startsWith('pre') && a.slice(3) === b;
+  return isRelatedScript(a, b, 'pre');
+}
+
+function isPostScriptFor(a, b) {
+  return isRelatedScript(a, b, 'post');
 }
 
 export default function compareScriptNames(a, b) {
-  if (isPreScriptFor(a, b)) {
-    return -1;
-  }
-
-  if (isPreScriptFor(b, a)) {
-    return 1;
-  }
+  if (isPreScriptFor(a, b)) return -1;
+  if (isPreScriptFor(b, a)) return 1;
+  if (isPostScriptFor(a, b)) return 1;
+  if (isPostScriptFor(b, a)) return -1;
 
   if ('test' === a) {
     return sortTestScript(a, b);
--- src/package/scripts/script-comparator.test.js
+++ src/package/scripts/script-comparator.test.js
@@ -4,26 +4,37 @@ import any from '@travi/any';
 import compareScriptNames from './script-comparator.js';
 
 describe('script name comparator', () => {
+  const A_AFTER_B = 1;
+  const A_BEFORE_B = -1;
+  const baseScriptName = any.word();
+
   it('should consider undefined sort orders as equivalent', async () => {
     expect(compareScriptNames(any.word(), any.word())).toEqual(0);
   });
 
   it('should sort `pre` scripts ahead of their related scripts', async () => {
-    const baseScriptName = any.word();
-    expect(compareScriptNames(`pre${baseScriptName}`, baseScriptName)).toEqual(-1);
-    expect(compareScriptNames(baseScriptName, `pre${baseScriptName}`)).toEqual(1);
+    expect(compareScriptNames(`pre${baseScriptName}`, baseScriptName)).toEqual(A_BEFORE_B);
+    expect(compareScriptNames(baseScriptName, `pre${baseScriptName}`)).toEqual(A_AFTER_B);
 
-    expect(compareScriptNames('pretest', 'test')).toEqual(-1);
-    expect(compareScriptNames('test', 'pretest')).toEqual(1);
+    expect(compareScriptNames('pretest', 'test')).toEqual(A_BEFORE_B);
+    expect(compareScriptNames('test', 'pretest')).toEqual(A_AFTER_B);
+  });
+
+  it('should sort `post` scripts after of their related scripts', async () => {
+    expect(compareScriptNames(`post${baseScriptName}`, baseScriptName)).toEqual(A_AFTER_B);
+    expect(compareScriptNames(baseScriptName, `post${baseScriptName}`)).toEqual(A_BEFORE_B);
+
+    expect(compareScriptNames('posttest', 'test')).toEqual(A_AFTER_B);
+    expect(compareScriptNames('test', 'posttest')).toEqual(A_BEFORE_B);
   });
 
   it('should sort the `test` script ahead of any sub-test scripts', async () => {
-    expect(compareScriptNames('test', `test:${any.word()}`)).toEqual(-1);
-    expect(compareScriptNames(`test:${any.word()}`, 'test')).toEqual(1);
+    expect(compareScriptNames('test', `test:${any.word()}`)).toEqual(A_BEFORE_B);
+    expect(compareScriptNames(`test:${any.word()}`, 'test')).toEqual(A_AFTER_B);
   });
 
   it('should sort `lint:` scripts above `test:` scripts', async () => {
-    expect(compareScriptNames(`lint:${any.word()}`, `test:${any.word()}`)).toEqual(-1);
-    expect(compareScriptNames(`test:${any.word()}`, `lint:${any.word()}`)).toEqual(1);
+    expect(compareScriptNames(`lint:${any.word()}`, `test:${any.word()}`)).toEqual(A_BEFORE_B);
+    expect(compareScriptNames(`test:${any.word()}`, `lint:${any.word()}`)).toEqual(A_AFTER_B);
   });
 });
