0e120ec04307beeef1f00ea0b00628fda5d89808
Matt Travi <programmer@travi.org>
2026-03-20 10:45:33 -0500

    test(unit): fix coverage colection

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,12 @@
 import {defineConfig} from 'vitest/config';
 
-export default defineConfig({test: {restoreMocks: true, mockReset: true}});
+export default defineConfig({test: {
+  restoreMocks: true,
+  mockReset: true,
+
+  coverage: {
+    provider: "v8",
+    include: ["src/**"],
+    exclude: ["src/**/index.js"]
+  }
+}});
